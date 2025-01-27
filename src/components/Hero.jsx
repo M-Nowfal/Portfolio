1308a12966bdcd638983277950ae0e9cff60aaca
Muhammed Nowfal <nowfalmmuhammed@gmail.com>
2025-01-27 21:08:57 +0530

    update3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,15 +12,31 @@ function Hero() {
             <div className="container">
                 <div className="row align-items-center text-center">
                     <div className="col-12 col-md-6 mb-4 mt-3" data-aos="fade-right">
-                        <img 
-                            src="Nowfal.png" 
-                            alt="Muhammed Nowfal" 
+                        <img
+                            src="Nowfal.png"
+                            alt="Muhammed Nowfal"
                             className="img-fluid hero-image"
                         />
                     </div>
                     <div className="col-12 col-md-6 mt-1" data-aos="fade-left">
                         <h1 className="m-2 name"><span className="text-white">I'm </span>Muhammed Nowfal</h1>
-                        <h3 className="m-2 roll"><span className="roll-in">I'm a </span>FULL STACK DEVELOPER</h3>
+                        <h3 className="m-2 roll"><span className="roll-in">I'm a </span>
+                            <Typewriter
+                                options={{
+                                    strings: [
+                                        "MERN Stack Developer",
+                                        "Full Stack Developer",
+                                        "Front-end Developer.",
+                                        "Back-end Developer.",
+                                        "Designer.",
+                                    ],
+                                    autoStart: true,
+                                    loop: true,
+                                    delay: 50,
+                                    deleteSpeed: 30,
+                                }}
+                            />
+                        </h3>
                         <button className="m-2 resume" onClick={downloadResume}>Download Resume</button>
                     </div>
                 </div>
